e29757f7e94809562a779fdf72b4a36563d413ca
Matvey Sergeev <47077074+Enhisir@users.noreply.github.com>
2021-05-02 08:31:31 +0500

    made tasks

---
+++ introduction/1.15.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define BEGIN 0
+#define END 300
+#define STEP 20
+
+double fahrenheit(int t);
+double celsius(int t);
+
+int main() {
+    int t;
+    
+    printf("Таблица температур (C -> F)\n");
+    for (t = BEGIN; t <= END; t += STEP) {
+        printf("%6d %6.2f\n", t, fahrenheit(t));
+    }
+    
+    printf("Таблица температур (F -> C)\n");
+    for (t = BEGIN; t <= END; t += STEP) {
+        printf("%6d %6.2f\n", t, celsius(t));
+    }
+
+    return 0;
+}
+
+double fahrenheit(int t) {
+    return 32 + 9.0 / 5.0 * t;
+}
+
+double celsius(int t) {
+    return 5.0 / 9.0 * (t - 32);
+}
---
+++ introduction/1.16.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define N 1000
+
+int _getline(char [], int);
+void copy(char [], char []);
+
+int main() {
+    int len, max, local;
+    char line[N], longest[N];
+
+    max = 0;
+    while ((len = _getline(line, N)) > 0) {
+        if (line[len - 1] != '\n') {
+            local = _getline(line, N);
+            while (local != 0 && line[local - 1] != '\n') {
+                if (local == 0)
+                    break;
+                len += local - 1;
+                local = _getline(line, N);
+            }
+            len += local - 1;
+        }
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    }
+    if (max > 0)
+        printf("Its length is %d\n", max);
+    return 0;
+}
+
+int _getline(char line[], int maxline) {
+    int i;
+    char c;
+
+    for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+    if (c == '\n')
+        line[i++] = c;
+    line[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+    int i;
+    
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
