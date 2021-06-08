69a387d60dfe3de9fd5530dbb18a2e5ab85adaab
Matvey Sergeev <47077074+Enhisir@users.noreply.github.com>
2021-06-08 18:55:06 +0500

    Update 2.1.c

--- types, operations, statements/2.1.c
+++ types, operations, statements/2.1.c
@@ -3,12 +3,12 @@
 
 int main() {
     printf("char variables vary from %d to %d\n", CHAR_MIN, CHAR_MAX);
-    printf("unsigned char variables vary from %ud to %u\n", 0, UCHAR_MAX);
+    printf("unsigned char variables vary from %d to %u\n", 0, UCHAR_MAX);
     printf("short variables vary from %d to %d\n", SHRT_MIN, SHRT_MAX);
-    printf("unsigned short variables vary from %u to %u\n", 0, USHRT_MAX);
+    printf("unsigned short variables vary from %d to %u\n", 0, USHRT_MAX);
     printf("int variables vary from %d to %d\n", INT_MIN, INT_MAX);
-    printf("unsigned int variables vary from %u to %u\n", 0, UINT_MAX);
-    printf("long variables vary from %d to %d\n", LONG_MIN, LONG_MAX);
-    printf("unsigned char variables vary from %lu to %lu\n", 0, ULONG_MAX);
+    printf("unsigned int variables vary from %d to %u\n", 0, UINT_MAX);
+    printf("long variables vary from %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("unsigned char variables vary from %d to %lu\n", 0, ULONG_MAX);
     return 0;
 }
